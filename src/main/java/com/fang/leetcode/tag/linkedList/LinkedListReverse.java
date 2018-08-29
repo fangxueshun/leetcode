3f6d98ff3faa8064e90d3f23d0d24ebe486cd2f4
fangxueshun <fangxueshun>
2018-08-29 21:40:13 +0800

    add "linked list reverse" problem

---
+++ src/main/java/com/fang/leetcode/tag/linkedList/LinkedListReverse.java
@@ -0,0 +1,25 @@
+package com.fang.leetcode.tag.linkedList;
+
+import com.fang.leetcode.tag.util.collection.linkList.UnidirectionalLinkedList;
+
+/**
+ * description
+ *
+ 反转一个单链表。
+
+ 示例:
+
+ 输入: 1->2->3->4->5->NULL
+ 输出: 5->4->3->2->1->NULL
+ 进阶:
+ 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * @author fangxueshun
+ * @date 2018/8/29
+ */
+public class LinkedListReverse {
+
+    public static UnidirectionalLinkedList reverseList(UnidirectionalLinkedList head) {
+        return head;
+    }
+
+}
---
+++ src/test/java/com/fang/leetcode/tag/linkedList/LinkedListReverseTest.java
@@ -0,0 +1,28 @@
+package com.fang.leetcode.tag.linkedList;
+
+import com.fang.leetcode.tag.util.collection.linkList.UnidirectionalLinkedList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * description
+ *
+ * @author fangxueshun
+ * @date 2018/8/29
+ */
+public class LinkedListReverseTest {
+
+    @Test
+    public void reverseList() {
+        UnidirectionalLinkedList linkedList = new UnidirectionalLinkedList();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        LinkedListReverse.reverseList(linkedList);
+        Assert.assertEquals("5->4->3->2->1",linkedList.toString());
+    }
+}
